c37fa06784aca5d9c3c65f3d917a81dcca4e65fa
sunilkumarbm <sunsda@gmail.com>
2018-01-26 23:58:02 +0530

    StringUtils functionality added

--- src/main/java/com/sunnytech/mavenquickstart/Application.java
+++ src/main/java/com/sunnytech/mavenquickstart/Application.java
@@ -1,8 +1,18 @@
 package com.sunnytech.mavenquickstart;
 
+import org.apache.commons.lang3.StringUtils;
+
 class Application {
+	public int countWords(String sentence) {
+		String words[] = StringUtils.split(sentence, " ");
+		return words == null ? 0 : words.length;
+	}
+
     public static void main(String ar[]) {
         System.out.println("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+
+		int count = app.countWords("hello sunil. have a nice day");
+		System.out.println("Count = " + count);
     }
 }
