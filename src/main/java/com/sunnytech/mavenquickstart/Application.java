d3dfc273892324198f1b5a65978f42f8ca8dd683
sunilkumarbm <sunsda@gmail.com>
2018-01-26 22:33:16 +0530

    First piece of code

---
+++ src/main/java/com/sunnytech/mavenquickstart/Application.java
@@ -0,0 +1,8 @@
+package com.sunnytech.mavenquickstart;
+
+class Application {
+    public static void main(String ar[]) {
+        System.out.println("Starting Application");
+	Application app = new Application();
+    }
+}
